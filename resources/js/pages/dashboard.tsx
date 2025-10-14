344ed2ec408da0ee3fd4256d0225ca33f300f18c
adiarhmn <adiauliarahmn@gmail.com>
2025-10-14 18:08:33 +0800

    add: deploy.yml

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -8,7 +8,7 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
-import data from "./data.json"
+import data from "../pages/admin/data-dummy/dashboard.json"
 
 export default function Page() {
   return (
